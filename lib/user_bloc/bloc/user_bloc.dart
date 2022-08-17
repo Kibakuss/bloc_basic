3e367e806aa24b84849e200593606d3876fd1882
kibakuss <duxabank@gmail.com>
2022-08-17 23:39:08 +0500

    interaction blocs

--- lib/user_bloc/bloc/user_bloc.dart
+++ lib/user_bloc/bloc/user_bloc.dart
@@ -1,13 +1,30 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_basic/counter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'user_event.dart';
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserState()) {
+  final CounterBloc counterBloc;
+  late final StreamSubscription counterBlocSubscription;
+  UserBloc(this.counterBloc) : super(UserState()) {
     on<UsersGetUserEvent>(_onGetUser);
     on<UsersGetUserJobEvent>(_onGetUserJob);
+    counterBlocSubscription = counterBloc.stream.listen((state) {
+      if (state <= 0) {
+        add(UsersGetUserEvent(0));
+        add(UsersGetUserJobEvent(0));
+      }
+    });
+
+    @override
+    Future<void> close() async {
+      counterBlocSubscription.cancel();
+      return super.close();
+    }
   }
 
   _onGetUser(UsersGetUserEvent event, Emitter<UserState> emit) async {
